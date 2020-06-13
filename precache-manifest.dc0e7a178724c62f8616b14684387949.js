f33d9b8fe4d827104a132f4c48da0693d5182c90
Gabriel Lopez <gabe.lop.lag@gmail.com>
2020-06-13 01:17:11 -0500

    Updates

---
+++ precache-manifest.dc0e7a178724c62f8616b14684387949.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ba5f243f3d0d1f9f35c1770163b682",
+    "url": "/dashboard-bi/index.html"
+  },
+  {
+    "revision": "b0c1b056f258716a2425",
+    "url": "/dashboard-bi/static/css/2.7cbeb696.chunk.css"
+  },
+  {
+    "revision": "538409067fb301c91f06",
+    "url": "/dashboard-bi/static/css/main.3947f261.chunk.css"
+  },
+  {
+    "revision": "b0c1b056f258716a2425",
+    "url": "/dashboard-bi/static/js/2.8af60368.chunk.js"
+  },
+  {
+    "revision": "27941b2a9e7bf240e8063a4f172250a6",
+    "url": "/dashboard-bi/static/js/2.8af60368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538409067fb301c91f06",
+    "url": "/dashboard-bi/static/js/main.5c2a093b.chunk.js"
+  },
+  {
+    "revision": "714b1feef5c8fbc4552f",
+    "url": "/dashboard-bi/static/js/runtime-main.424992f2.js"
+  }
+]);
